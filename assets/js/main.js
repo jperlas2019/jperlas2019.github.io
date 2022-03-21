2265ea77aa8a9cd38e2e674d8c046e0d83f72d60
Jaguar Perlas <jperlas@my.bcit.ca>
2022-03-20 23:08:51 -0700

    March 2022 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,28 @@
 !(function($) {
   "use strict";
 
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 40,
+      backSpeed: 20,
+      backDelay: 2000
+    });
+  }
+
   // Nav Menu
   $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
